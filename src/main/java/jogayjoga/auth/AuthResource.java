2bd3213f0f0cd2794c07b3580ca678e433228fc1
eriksoaress <erikbtu2020@gmail.com>
2024-04-05 16:26:24 -0300

    pom

--- src/main/java/jogayjoga/auth/AuthResource.java
+++ src/main/java/jogayjoga/auth/AuthResource.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
+
+
 @RestController
 public class AuthResource implements AuthController {
     @Autowired
