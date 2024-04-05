ec2aad97ffb05b7c745927c10884bcee021f8207
eriksoaress <erikbtu2020@gmail.com>
2024-04-05 16:48:40 -0300

    application base packages

--- src/main/java/jogayjoga/auth/AuthApplication.java
+++ src/main/java/jogayjoga/auth/AuthApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-@EnableFeignClients(basePackages = {"insper.ingressify.account"})
+@EnableFeignClients(basePackages = {"jogayjoga.account"})
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AuthApplication {
